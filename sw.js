8627c1632669299ce564c47fff8d92e96aa37755
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-08 13:40:01 +1000

    Update service worker for static v0.8.4

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const CACHE='mesh-line-replacer-v0.8.3-torus-instances';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
+const CACHE='mesh-line-replacer-v0.8.4-static-nomad';
+const APP_SHELL=[
+  './','./index.html','./manifest.webmanifest',
+  './app-v084-static/part00.txt','./app-v084-static/part01.txt','./app-v084-static/part02.txt','./app-v084-static/part03.txt','./app-v084-static/part04.txt',
+  './app-v084-static/part05.txt','./app-v084-static/part06.txt','./app-v084-static/part07.txt','./app-v084-static/part08.txt','./app-v084-static/part09.txt'
+];
 self.addEventListener('install',(event)=>event.waitUntil(caches.open(CACHE).then((cache)=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',(event)=>event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((key)=>key!==CACHE).map((key)=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',(event)=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then((cached)=>cached||fetch(event.request).then((response)=>{if(response&&(response.status===200||response.type==='opaque'))caches.open(CACHE).then((cache)=>cache.put(event.request,response.clone()));return response;}).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('fetch',(event)=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(caches.match(event.request).then((cached)=>cached||fetch(event.request).then((response)=>{
+    if(response&&(response.status===200||response.type==='opaque'))caches.open(CACHE).then((cache)=>cache.put(event.request,response.clone()));
+    return response;
+  }).catch(()=>caches.match('./index.html'))));
+});
