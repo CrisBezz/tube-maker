ae8325650ad78d4eee15acdbea6a0e8395595ac3
Bezzo <115067958+CrisBezz@users.noreply.github.com>
2026-08-08 20:37:05 +1000

    Refresh v0.8.9 density cache

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-const CACHE='mesh-line-replacer-v0.8.8-view-modes';
+const CACHE='mesh-line-replacer-v0.8.9-density';
 const APP_SHELL=[
   './','./index.html','./manifest.webmanifest',
   './wip/app-v086-static/part00.txt','./wip/app-v086-static/part01.txt','./wip/app-v086-static/part02.txt','./wip/app-v086-static/part03.txt','./wip/app-v086-static/part04.txt',
   './wip/app-v086-static/part05.txt','./wip/app-v086-static/part06.txt','./wip/app-v086-static/part07.txt','./wip/app-v086-static/part08.txt','./wip/app-v086-static/part08b.txt','./wip/app-v086-static/part09.txt',
-  './wip/app-v087-static/grouped-instances.txt',
-  './wip/app-v088-static/view-modes.txt'
+  './wip/app-v087-static/grouped-instances.txt','./wip/app-v088-static/view-modes.txt','./wip/app-v089-static/instance-density.txt','./wip/app-v089-static/density-v2-override.txt'
 ];
 self.addEventListener('install',(event)=>event.waitUntil(caches.open(CACHE).then((cache)=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',(event)=>event.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((key)=>key!==CACHE).map((key)=>caches.delete(key)))).then(()=>self.clients.claim())));
